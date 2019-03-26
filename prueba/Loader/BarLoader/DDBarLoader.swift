8d5fa01b3af66ad0ddc5949bef46668b312244fb
David Diego Gomez <diegodavid@icloud.com>
2019-03-25 21:04:51 -0300

    d

--- prueba/Loader/BarLoader/DDBarLoader.swift
+++ prueba/Loader/BarLoader/DDBarLoader.swift
@@ -197,3 +197,60 @@ extension DDBarLoader {
     }
 }
 
+extension UIView {
+    
+    public func pauseAnimation(delay: Double) {
+        let time = delay + CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, time, 0, 0, 0, { timer in
+            let layer = self.layer
+            let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+            layer.speed = 0.0
+            layer.timeOffset = pausedTime
+        })
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
+    }
+    
+    public func resumeAnimation() {
+        let pausedTime  = layer.timeOffset
+        
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+    }
+    
+    func slide(fromX: CGFloat?, toX: CGFloat, duration: TimeInterval) {
+        
+        let yPosition = self.frame.origin.y
+        
+        let height = self.frame.height
+        let width = self.layer.frame.size.width
+        
+        
+        if fromX != nil {
+            self.frame = CGRect(x: fromX!, y: yPosition, width: width, height: height)
+        }
+        UIView.animate(withDuration: duration, delay: 0.0, options: [.curveEaseInOut], animations: {
+            self.frame = CGRect(x: toX, y: yPosition, width: width, height: height)
+            
+        })
+    }
+    
+    func slide(fromX: CGFloat?, toX: CGFloat, duration: TimeInterval, completion: @escaping () -> Void) {
+        
+        let yPosition = self.frame.origin.y
+        
+        let height = self.frame.height
+        let width = self.layer.frame.size.width
+        
+        if fromX != nil {
+            self.frame = CGRect(x: fromX!, y: yPosition, width: width, height: height)
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: [.curveEaseInOut], animations: {
+            self.frame = CGRect(x: toX, y: yPosition, width: width, height: height)
+            
+        }) { (finished) in
+            completion()
+        }
+    }
+}
